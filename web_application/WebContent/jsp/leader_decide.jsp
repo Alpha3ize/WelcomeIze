8e4216c676ea0ebf6f965494c51d2e28c2fe618b
森田 <morita_nanase@3-ize.jp>
2023-05-24 18:29:18 +0900

    途中

--- web_application/WebContent/jsp/leader_decide.jsp
+++ web_application/WebContent/jsp/leader_decide.jsp
@@ -4,9 +4,13 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>リーダー登録</title>
 </head>
 <body>
-
+<h2>リーダー登録</h2>
+<table>
+<tr></tr>
+<button type="button" name="leader">リーダーにする</button>
+</table>
 </body>
 </html>
--- web_application/src/jp/co/alpha/servlet/admin/LeaderDecide.java
+++ web_application/src/jp/co/alpha/servlet/admin/LeaderDecide.java
@@ -1,12 +1,20 @@
 package jp.co.alpha.servlet.admin;
 
+import java.rmi.ServerException;
+import java.rmi.server.ServerCloneException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class LeaderDecide
  */
 @WebServlet("/leaderDecide")
 public class LeaderDecide extends HttpServlet {
-	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServerException IOException{
+		
+	}
 }
