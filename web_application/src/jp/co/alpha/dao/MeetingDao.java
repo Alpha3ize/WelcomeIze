52d15d15cfb82aa7eae36392e20fdeff9f1c9ad9
Shimizu <shimizu_kaito@3-ize.jp>
2023-05-24 13:53:45 +0900

    いろいろ完成版

--- web_application/src/jp/co/alpha/dao/MeetingDao.java
+++ web_application/src/jp/co/alpha/dao/MeetingDao.java
@@ -7,45 +7,37 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import jp.co.sss.beanTw.Tweets;
-import jp.co.sss.dao.DBManagerTw;
+import jp.co.alpha.bean.Meeting;
+import jp.co.alpha.dao.DBManager;
 
 public class MeetingDao {
-	public static List<Meeting> () {
+	public static void insertMeet(Meeting meet) {
 		Connection con = null;
 		PreparedStatement ps = null;
-		List<Meeting> tweetList = new ArrayList<>();
 		try {
-			con = DBManagerTw.getConnection();
-			ps = con.prepareStatement(
-					"SELECT t.id, t.body, t.post_date, t.user_id, u.name FROM tweets t INNER JOIN users u ON t.user_id = u.id ORDER BY t.post_date DESC");
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				Tweets tweet = new Tweets();
-				tweet.setId(Integer.parseInt(rs.getString("id")));
-				tweet.setBody(rs.getString("body"));
-				tweet.setPostDate(rs.getString("post_date"));
-				tweet.setUserId(Integer.parseInt(rs.getString("user_id")));
-				tweet.setName(rs.getString("name"));
-				tweetList.add(tweet);
-			}
+			con = DBManager.getConnection();
+			ps = con.prepareStatement("INSERT INTO meeting VALUES(シーケンス.NEXTVAL,?,?,?,?,0)");
+			ps.setString(1, meet.getM_Date());
+			ps.setString(2, meet.getM_Date_End());
+			ps.setInt(3, meet.getM_Name());
+			ps.setInt(4, meet.getM_Place());
+			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			DBManagerTw.close(ps, con);
+			DBManager.close(ps, con);
 		}
-		return tweetList;
 	}
 
-	public static void insert(Tweets tw) {
+	public static void updateMeet(Meeting meet) {
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
-			con = DBManagerTw.getConnection();
-			ps = con.prepareStatement("INSERT INTO tweets VALUES(users_seq.NEXTVAL,?,?,?)");
-			ps.setString(1, tw.getBody());
-			System.out.println(tw.getPostDate());
-			ps.setString(2, tw.getPostDate());
+			con = DBManager.getConnection();
+			ps = con.prepareStatement("UPDATE meeting SET m_date = ?, m_date_end = ?, m_name = ?, m_place = ? where m_id = ?");
+			ps.setString(1, meet.getBody());
+			System.out.println(meet.getPostDate());
+			ps.setString(2, meet.getPostDate());
 			ps.setInt(3, tw.getUserId());
 			ps.executeUpdate();
 		} catch (SQLException e) {
