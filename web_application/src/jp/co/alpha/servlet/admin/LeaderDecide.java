6badb7300a0b8593fcaf1b7f6ae1ffac9b85facf
森田 <morita_nanase@3-ize.jp>
2023-05-24 18:20:18 +0900

    管理者用サーブレット

---
+++ web_application/src/jp/co/alpha/servlet/admin/LeaderDecide.java
@@ -0,0 +1,12 @@
+package jp.co.alpha.servlet.admin;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class LeaderDecide
+ */
+@WebServlet("/leaderDecide")
+public class LeaderDecide extends HttpServlet {
+	
+}
